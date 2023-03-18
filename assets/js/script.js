d3f8afc2003beeec126cf3344d4ad0d6ca957073
Paul Co <paulteves353@gmail.com>
2023-03-18 16:23:06 +0800

    Update

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $('.next-image').click(function() {
+        var currentImg = $(this).siblings('.project-images').find('img.active');
+        var nextImg = currentImg.next();
+        if(nextImg.length) {
+            currentImg.removeClass('active');
+            nextImg.addClass('active');
+        }
+    });
+  
+    $('.prev-image').click(function() {
+        var currentImg = $(this).siblings('.project-images').find('img.active');
+        var prevImg = currentImg.prev();
+        if (prevImg.length) {
+            currentImg.removeClass('active');
+            prevImg.addClass('active');
+        }
+    });
+});
