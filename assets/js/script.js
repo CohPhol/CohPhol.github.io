e9d2f172606e03ba5693966d67e4db1e2ba4eb90
Paul Co <paulteves353@gmail.com>
2023-03-18 21:38:39 +0800

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,32 @@
 
-$(document).ready(function() {
-    $('.next-image').click(function() {
+$(document).ready(function(){
+    // Image start
+    $('.next-image').click(function(){
         var currentImg = $(this).siblings('.project-images').find('img.active');
         var nextImg = currentImg.next();
-        if(nextImg.length) {
+        if(nextImg.length){
             currentImg.removeClass('active');
             nextImg.addClass('active');
         }
     });
   
-    $('.prev-image').click(function() {
+    $('.prev-image').click(function(){
         var currentImg = $(this).siblings('.project-images').find('img.active');
         var prevImg = currentImg.prev();
-        if (prevImg.length) {
+        if(prevImg.length){
             currentImg.removeClass('active');
             prevImg.addClass('active');
         }
+    });
+    // Image end
+
+    // Progress bar
+    $('.progress-bar').each(function() {
+        var $circle = $(this);
+        var percentage = parseInt($circle.parent().find('.progress-num').text());
+        var radius = $circle.attr('r');
+        var circumference = 2 * Math.PI * radius;
+        var offset = circumference - percentage / 100 * circumference;
+        $circle.css({'stroke-dasharray': circumference + ' ' + circumference, 'stroke-dashoffset': offset});
     });
 });
